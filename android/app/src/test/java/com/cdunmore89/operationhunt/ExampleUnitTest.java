fe3423dc6e9aedc5c94201f03bfb8472b723bb6b
cdunmore89-eng <cdunmore89@gmail.com>
2026-07-27 01:02:37 -0500

    Align Android unit test namespace

---
+++ android/app/src/test/java/com/cdunmore89/operationhunt/ExampleUnitTest.java
@@ -0,0 +1,16 @@
+package com.cdunmore89.operationhunt;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Example local unit test that runs on the development machine.
+ */
+public class ExampleUnitTest {
+
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
